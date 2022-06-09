315b5802ad51feb53df4b21234438ef63f89077d
Lucas Bastidas <lucaasbastidas@hotmail.com>
2022-06-09 14:25:56 -0300

    key.json remove

--- server/database.ts
+++ server/database.ts
@@ -1,8 +1,10 @@
+import { json } from "body-parser";
 import * as admin from "firebase-admin";
-import * as serviceAccount from "./key.json";
+// import * as serviceAccount from "./key.json";
 
+const serviceAccount = JSON.parse(process.env.GOOGLE_CREDS);
 admin.initializeApp({
-	credential: admin.credential.cert(serviceAccount as any),
+	credential: admin.credential.cert(serviceAccount),
 	databaseURL: "https://apx-dwf-m6-72162-default-rtdb.firebaseio.com",
 });
 const firestore = admin.firestore();
