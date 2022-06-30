6401c0e3bf03ea05e6343abe9074b7465fe6f49f
Lucas Bastidas <lucaasbastidas@hotmail.com>
2022-06-30 01:39:19 -0300

    commit

--- client/router.ts
+++ client/router.ts
@@ -1,6 +1,6 @@
 import { Router } from "@vaadin/router";
 import { state } from "./state";
-
+//Router
 const router = new Router(document.querySelector(".root"));
 router.setRoutes([
 	{ path: "/", component: "x-home-page" },
